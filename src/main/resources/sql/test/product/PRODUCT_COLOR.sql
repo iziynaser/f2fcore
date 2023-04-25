1d6b8985e4ed44fc7c2a5c20149dad5558824101
iziy <iziy_naser@yahoo.com>
2023-04-25 18:21:38 +0330

    add sample data for product color

---
+++ src/main/resources/sql/test/product/PRODUCT_COLOR.sql
@@ -0,0 +1,23 @@
+    INSERT INTO public.product_color(
+	id, color_name, product_id, color_value)
+	VALUES (1,'blue',1,'blue');
+
+	INSERT INTO public.product_color(
+	id, color_name, product_id, color_value)
+	VALUES (2,'red',1,'red');
+
+	INSERT INTO public.product_color(
+	id, color_name, product_id, color_value)
+	VALUES (3,'green',1,'green');
+
+	INSERT INTO public.product_color(
+	id, color_name, product_id, color_value)
+	VALUES (4,'gray',1,'gray');
+
+	INSERT INTO public.product_color(
+	id, color_name, product_id, color_value)
+	VALUES (5,'blue',2,'blue');
+
+	INSERT INTO public.product_color(
+	id, color_name, product_id, color_value)
+	VALUES (6,'red',2,'red');
