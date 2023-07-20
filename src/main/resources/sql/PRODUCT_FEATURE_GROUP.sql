381b38f75691fff8deb358678401a51c651a0531
iziy <iziy_naser@yahoo.com>
2023-07-20 16:27:09 +0330

    improve :        updating sql script commands

---
+++ src/main/resources/sql/PRODUCT_FEATURE_GROUP.sql
@@ -0,0 +1,12 @@
+INSERT INTO public.pr_feature_group(
+	id, date_created, last_modified,
+	record_status, remarks,
+	sort_priority, version,
+	feature_value,feature_name,
+	orders, product_id,
+	feature_group_value, feature_group_name)
+	VALUES (6, null, null,
+			null, null,
+			null, 0,
+			0, 3, 3, 3,
+			'moshakhasat', 'moshakhasat');
---
+++ src/main/resources/sql/PRODUCT_FEATURE_ITEM.sql
@@ -0,0 +1,23 @@
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(1,'ارتباطات','ارتباطات',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(2,'دوربين','دوربين',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(3,'صدا','صدا',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(4,'امکانات نرم افزاري','امکانات نرم افزاري',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(5,'صفحه نمايش','صفحه نمايش',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(6,'حافظه','حافظه',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(7,'پردازنده','پردازنده',1);
+
+insert into PR_FEATURE_ITEM(id,feature_item_name,feature_item_value,feature_group_id)
+values(8,'مشخصات کلي','مشخصات کلي',1);
