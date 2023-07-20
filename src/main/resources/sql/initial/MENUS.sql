82507a91c81fcdbb6132901d8cf54bfd53df6f85
iziy <iziy_naser@yahoo.com>
2023-07-20 14:58:32 +0330

    bug :         correct sql script for menus and content

---
+++ src/main/resources/sql/initial/MENUS.sql
@@ -0,0 +1,9 @@
+INSERT INTO public.category(
+	id, date_created, last_modified, record_status,
+	remarks, sort_priority, version, category_code,
+	description, detail_image_url, from_date,
+	image_url, name, to_date, category_type_id,parent_category_id)
+	VALUES (58, null, null, 0 ,
+			null , 0 , 0 , 9 ,
+			'menu1', null, null ,
+			null , 'menu1', null , 9, 1);
--- src/main/resources/sql/initial/PRODUCT_CATEGORY_TYPE.sql
+++ src/main/resources/sql/initial/PRODUCT_CATEGORY_TYPE.sql
@@ -1,21 +1,21 @@
 insert into product_category_type(id,name,value)
-value (1,'تصویر','تصویر')
+values (1,'تصویر','تصویر');
 
 insert into product_category_type(id,name,value)
-value (2,'ویدئو','ویدئو')
+values (2,'ویدئو','ویدئو');
 
 insert into product_category_type(id,name,value)
-value (3,'شرح','شرح')
+values (3,'شرح','شرح');
 
 insert into product_category_type(id,name,value)
-value (4,'کلمات کلیدی','کلمات کلیدی')
+values (4,'کلمات کلیدی','کلمات کلیدی');
 
 insert into product_category_type(id,name,value)
-value (5,'هدر','هدر')
+values (5,'هدر','هدر');
 
 insert into product_category_type(id,name,value)
-value (6,'فوتر','فوتر')
+values (6,'فوتر','فوتر');
 
 insert into product_category_type(id,name,value)
-value (7,'لینک','لینک')
+values (7,'لینک','لینک');
 
