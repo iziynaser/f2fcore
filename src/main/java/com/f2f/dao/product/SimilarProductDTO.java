e4097634f9b41e9cf87c23eee079ec2d8679a563
iziy <iziy_naser@yahoo.com>
2023-07-23 15:09:48 +0330

    create similarProductDTO
    remover similarProductDTO and add the class

---
+++ src/main/java/com/f2f/dao/product/SimilarProductDTO.java
@@ -0,0 +1,10 @@
+package com.f2f.dao.product;
+
+public interface SimilarProductDTO {
+    public  String getProductId();
+    public String getPrice();
+    public String getRealPrice();
+    public String getNumberOfRemains();
+
+
+}
